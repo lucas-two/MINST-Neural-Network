9651a22e94d9b4949bf102f6316829c0d0089a5e
lucas-two <43457360+lucas-two@users.noreply.github.com>
2019-05-12 21:54:17 +1000

    Need fixes on the hidden_calc_e_total method

--- NeuralNet.py
+++ NeuralNet.py
@@ -88,8 +88,9 @@ def out(input_lst, weight_lst, bias):
 
 def out_calc_e_total(target, fp_lst):
     """
-    Calculates the e_total for weights connected to the output.
+    Calculates the e_total for weights connected from hidden to output.
     This is done using the following formula:
+
     ETotal = -(TargetOut - OutputOut) * OutputOut * (1 - OutputOut) * OutputOut
     """
     e_total_list = []
@@ -104,15 +105,62 @@ def out_calc_e_total(target, fp_lst):
 
     return e_total_list
 
-def hidden_calc_e_total():
-    return
-# for hidden -> input
-# For all output nodes...
-# sum E_Outs += -(Target[out_node] - Out[out_node) * Out[out_node] * (1 - Out[out_node]) * weight
-# Etotal = sum E_Outs * Out[hidden] * (1 - Out[hidden]) * Out[input value]
+
+def hidden_calc_e_total(target, fp_lst, input_vals):
+    """
+    Calculates the e_total for weights connected from input to hidden layer.
+    This is done using the following formula:
+
+    SumEOuts = SUM OF ALL OUTPUT NODES -> -(TargetOut - OutputOut) * OutputOut * (1 - OutputOut) * Weight
+    ETotal = SumEOuts * HiddenOut * (1 - HiddenOut) * InputValue
+    """
+
+    # NOTE:
+    # Brain has lost ability to think.
+    # Will come back to solving this tomorrow.
+
+    # e_total_list = []
+    # e_sum_values = []
+
+    # for in_node in range(n_input):
+    #     e_out_sum = 0
+    #     for out_node in range(n_output):
+    #         e_out_sum += -1 * (target[out_node] - fp_lst[1][out_node]) * fp_lst[1][out_node] * (
+    #                 1 - fp_lst[1][out_node]) * input_weights[in_node + out_node]
+    #
+    #     for hidden_node in range(n_hidden):
+    #
+    # for weight in range(len(input_weights)):
+    #     for in_node in range(n_input):
+    #         e_out_sum = 0
+    #         for out_node in range(n_output):
+    #             e_out_sum += -1 * (target[out_node] - fp_lst[1][out_node]) * fp_lst[1][out_node] * (
+    #                     1 - fp_lst[1][out_node]) * input_weights[num]
+    #             print("Weight Used:", input_weights[num])
+
+
+    #
+    #
+    # for num in range(input_weights):  # For all weights
+    #
+    #     e_out_sum = 0
+    #
+    #     for out_node in range(n_output):  # For all output nodes
+    #         # Find the sum of outs
+    #         e_out_sum += -1 * (target[out_node] - fp_lst[1][out_node]) * fp_lst[1][out_node] * (
+    #                 1 - fp_lst[1][out_node]) * input_weights[num]
+    #         print("Weight Used:", input_weights[num])
+    #
+    #     for in_node in range(n_input):
+    #         e_total = e_out_sum * fp_lst[0][in_node] * (1 - fp_lst[0][in_node]) * input_vals[in_node]
+    #         e_total_list.append(e_total)
+    #
+    # return e_total_list
+
 
 def calc_e_final():
     return
+
 
 def calc_new_weight():
     return
@@ -122,9 +170,20 @@ def calc_new_weight():
 batch_no = 0
 
 forward_pass_list = forward_pass(batch_no)
-e_totals_hidden = out_calc_e_total(target_values[batch_no], forward_pass_list)
-print(e_totals_hidden)
+e_totals_output = out_calc_e_total(target_values[batch_no], forward_pass_list)
+e_total_hidden = hidden_calc_e_total(target_values[batch_no], forward_pass_list, input_values[batch_no])
+print(e_total_hidden)
 
+# For all output nodes...
+# sum E_Outs += -(Target[out_node] - Out[out_node) * Out[out_node] * (1 - Out[out_node]) * weight
+# Etotal = sum E_Outs * Out[hidden] * (1 - Out[hidden]) * Out[input value]
+
+
+
+
+
+    # e_out_sum += -1 * (target_values[batch_no] - )
+    # e_out_sum += -1 * Target[]
 
 # for _ in range(epoch):
 #     for _ in range(sample_size / batch_size):
