b502785b09d349bc74b2fc2ee0f994bc2c30cb81
lucas-two <43457360+lucas-two@users.noreply.github.com>
2019-05-14 17:23:02 +1000

    Bug fixes

--- NeuralNet.py
+++ NeuralNet.py
@@ -46,10 +46,11 @@ def forward_pass(batch_num, input_vals, input_bias):
     lower = 0
     upper = n_hidden
 
+    loop_counter = 0
     for node in range(n_output):
         output_out.append(out(hidden_out, hidden_weights[lower:upper], input_bias))
-        lower += n_input
-        upper += n_input
+        lower += n_hidden
+        upper += n_hidden
 
     forward_pass_result.append(output_out)
 
@@ -88,7 +89,6 @@ def out_calc_e_total(target, fp_lst):
 
     for in_node in range(n_hidden):  # For each hidden layer node
         for out_node in range(n_output):  # Find ETotal for weight connected to each output node
-
             e_total = -1 * (target[out_node] - fp_lst[1][out_node]) * fp_lst[1][out_node] * (
                         1 - fp_lst[1][out_node]) * fp_lst[0][in_node]
 
@@ -120,10 +120,10 @@ def hidden_calc_e_total(target, fp_lst, input_vals):
         e_sum_lst.append(e_sum)
 
     # Calculate ETotal for each weight
-    for input_node in range(n_input):
+    for input_node in range(n_input):  # 784
         for hidden_node in range(n_hidden):
-            e_total = e_sum_lst[hidden_node] * fp_lst[1][hidden_node] * (
-                    1 - fp_lst[1][hidden_node]) * input_vals[input_node]
+            e_total = e_sum_lst[hidden_node] * fp_lst[0][hidden_node] * (
+                    1 - fp_lst[0][hidden_node]) * input_vals[input_node]
 
             e_total_lst.append(e_total)
 
@@ -191,17 +191,42 @@ def cross_entropy_cost(output_out, target_out):
 
 
 # INPUT
-n_input = 10
-n_hidden = 3
-n_output = 4
+n_input = 784
+n_hidden = 30
+n_output = 10
+training_set_f = "TrainDigitX.csv.gz"
+training_target_f = "TrainDigitY.csv.gz"
 
-trainset  # Get file
+# Hyper Parameters
+epoch = 30
+sample_size = 50000
+batch_size = 20  # Mini batch
+learning_rate = 3
+my_bias = 0.01
 
-trainset_labels  # Get file
-# We should convert this to an array format
+# Grab the training target values
+training_target = np.loadtxt(training_target_f, dtype=int, delimiter=',')
 
-testset  # Get file
-testset_predictions = 0  # Save file
+target_values = []
+# Convert the training target values into a matrix
+for i in training_target:
+    data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    data[i] = 1
+    target_values.append(data)
+
+
+# Grab the training input values
+input_values = np.loadtxt(training_set_f, dtype=float, delimiter=',')
+
+# Shuffle the training data
+shuffled_set = list(zip(target_values, input_values))
+random.shuffle(shuffled_set)
+target_values, input_values = zip(*shuffled_set)
+target_values = list(target_values)
+input_values = list(input_values)
+
+# testset  # Get file
+# testset_predictions = 0  # Save file
 
 # First four weights = Input, next four weights = hidden
 # input_values = [[0.1, 0.1], [0.1, 0.2]]
@@ -209,45 +234,39 @@ testset_predictions = 0  # Save file
 # input_weights = [0.1, 0.1, 0.2, 0.1]
 # hidden_weights = [0.1, 0.1, 0.1, 0.2]
 
-# Hyper Parameters
-epoch = 30
-sample_size = 2
-batch_size = 2  # Mini batch
-learning_rate = 3
-my_bias = 0.1
-
 # Initialise the weights as random small float values, then split them into groups
 initial_weights = init_weights(n_input, n_hidden, n_output)
 input_weights = initial_weights[0:n_input * n_hidden]
 hidden_weights = initial_weights[n_input * n_hidden: n_input * n_hidden + n_hidden * n_output]
 
+for ep in range(epoch):
+    for bi in range(round(sample_size / batch_size)):
 
-# for ep in range(epoch):
-#     for bi in range(round(sample_size / batch_size)):
-#
-#         collected_e_total = []
-#         for batch_no in range(batch_size):
-#
-#             # Forward pass
-#             forward_pass_list = forward_pass(batch_no, input_values, my_bias)
-#
-#             # Calculate ETotal
-#             e_totals_output = out_calc_e_total(target_values[batch_no], forward_pass_list)
-#             e_total_hidden = hidden_calc_e_total(target_values[batch_no], forward_pass_list, input_values[batch_no])
-#             combined_e_total = e_total_hidden + e_totals_output
-#             collected_e_total.append(combined_e_total)
-#
-#         # Calculate EFinal
-#         e_final_list = calc_e_final(collected_e_total, batch_size)
-#
-#         # Calculate new weight total
-#         combined_weights = input_weights + hidden_weights
-#         my_new_weights = calc_new_weight(combined_weights, e_final_list, learning_rate)
-#
-#         print(my_new_weights)
-#         # Split weights up again
-#         input_weights = my_new_weights[0:len(input_weights)]
-#         hidden_weights = my_new_weights[len(input_weights): len(input_weights) + len(hidden_weights)]
+        collected_e_total = []
+        for batch_no in range(batch_size):
+
+            # Forward pass
+            forward_pass_list = forward_pass(batch_no, input_values, my_bias)
+
+            # Calculate ETotal
+            e_totals_output = out_calc_e_total(target_values[batch_no], forward_pass_list)
+            e_total_hidden = hidden_calc_e_total(target_values[batch_no], forward_pass_list, input_values[batch_no])
+            combined_e_total = e_total_hidden + e_totals_output
+            collected_e_total.append(combined_e_total)
+
+        # Calculate EFinal
+        e_final_list = calc_e_final(collected_e_total, batch_size)
+
+        # Calculate new weight total
+        combined_weights = input_weights + hidden_weights
+        my_new_weights = calc_new_weight(combined_weights, e_final_list, learning_rate)
+
+        # Split weights up again
+        input_weights = my_new_weights[0:len(input_weights)]
+        hidden_weights = my_new_weights[len(input_weights): len(input_weights) + len(hidden_weights)]
+        print("Successfully completed batch %s of %s" % (bi + 1, round((sample_size/batch_size))))
+
+    print("Successfully completed epoch %s of %s" % (ep + 1, epoch))
 
     # Predictions & Make update plot
     # fp = forward_pass(0, test_set, my_bias)  # Forward pass
